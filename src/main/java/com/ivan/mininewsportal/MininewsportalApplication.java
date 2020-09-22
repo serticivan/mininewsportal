714b17d490d8642f19826b4ec071343c16c4ff8e
ivan <ivansertic7@gmail.com>
2020-09-22 10:03:54 +0200

    initial commit

---
+++ src/main/java/com/ivan/mininewsportal/MininewsportalApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.mininewsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MininewsportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MininewsportalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivan/mininewsportal/MininewsportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.mininewsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MininewsportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
