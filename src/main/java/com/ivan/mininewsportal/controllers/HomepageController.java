501a682a0c694c2aaf88589e73d2a70c990c3487
ivan <ivansertic7@gmail.com>
2020-09-23 16:31:44 +0200

    added homepage

--- src/main/java/com/ivan/mininewsportal/controllers/HomepageController.java
+++ src/main/java/com/ivan/mininewsportal/controllers/HomepageController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomepageController {
 
+
+
     @RequestMapping({"/", "homepage", ""})
     private String showHomepage() {
-
         return "homepage";
 
     }
